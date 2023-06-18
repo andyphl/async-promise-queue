b251937ec9b299d974394fb4635d5e1e94ef388b
andyphl <phlai951016@gmail.com>
2023-06-18 21:11:43 +0900

    adding onSuccess, onError, onRetry options

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import process from "process";
 
+type Task<T> = {
+  task: () => Promise<T>;
+  index: number;
+  onSuccess?: (value: T) => void;
+  onError?: (err: Error) => void;
+  onRetry?: (err: Error) => void;
+};
 export class AsyncRequestQueue {
-  #queue: {
-    task: () => Promise<unknown>;
-    index: number;
-  }[];
+  #queue: Task<any>[];
   #currentTask: number;
   #currentIndex: number;
   retries: number;
   maxConcurrent: number;
 
-  constructor({ maxConcurrent = 3, retries = 3 }) {
+  constructor({
+    maxConcurrent = 3,
+    retries = 3,
+  }: {
+    maxConcurrent?: number;
+    retries?: number;
+  }) {
     this.#queue = [];
     this.#currentIndex = 0;
     this.#currentTask = 0;
@@ -18,38 +28,45 @@ export class AsyncRequestQueue {
     this.retries = retries;
   }
 
-  async enqueue(promiseFactory: () => Promise<unknown>) {
+  async enqueue<T>(
+    promiseFactory: () => Promise<T>,
+    {
+      onRetry,
+      onError,
+      onSuccess,
+    }: Pick<Task<T>, "onSuccess" | "onError" | "onRetry"> = {}
+  ) {
     this.#queue.push({
       task: promiseFactory,
       index: this.#currentIndex++,
+      onRetry,
+      onError,
+      onSuccess: onSuccess as (value: T) => void,
     });
     await this.#executeNext();
   }
 
-  async #loadWithRetry(
-    task: {
-      task: () => Promise<unknown>;
-      index: number;
-    },
-    retryCount: number
-  ) {
+  async #loadWithRetry(taskObj: Task<any>, retryCount: number) {
     try {
       if (retryCount === this.retries) {
         this.#currentTask++;
       }
-      console.log(`task ${task.index} start`);
-      await task.task();
+      console.log(`task ${taskObj.index} start`);
+      return await taskObj.task();
     } catch (err) {
       if (retryCount <= 0) {
         throw new Error(
-          `Task ${task.index} failed to load after ${this.retries} retries`
+          `Task ${taskObj.index} failed to load after ${this.retries} retries`
         );
       }
+      taskObj.onRetry?.(err as Error);
       console.log(
-        `Task ${task.index} retry for ${retryCount} times, after 1 second`
+        `Task ${taskObj.index} retry for ${
+          this.retries - retryCount + 1
+        } times, after 1 second`
       );
       await new Promise((resolve) => setTimeout(resolve, 1000));
-      await this.#loadWithRetry(task, retryCount - 1);
+      await this.#loadWithRetry(taskObj, retryCount - 1);
     }
   }
 
@@ -57,11 +74,13 @@ export class AsyncRequestQueue {
     if (this.#currentTask >= this.maxConcurrent) {
       return;
     }
-    const task = this.#queue.shift();
-    if (task) {
+    const taskObj = this.#queue.shift();
+    if (taskObj) {
       try {
-        await this.#loadWithRetry(task, this.retries);
+        const taskValue = await this.#loadWithRetry(taskObj, this.retries);
+        taskObj.onSuccess?.(taskValue);
       } catch (err) {
+        taskObj.onError?.(err as Error);
       } finally {
         this.#currentTask--;
         this.#executeNext();
@@ -71,7 +90,7 @@ export class AsyncRequestQueue {
 }
 
 const sleep = (ms: number) => {
-  return new Promise((resolve) => {
+  return new Promise<number>((resolve) => {
     setTimeout(() => {
       console.log(`sleep for ${ms / 1000} seconds`);
       resolve(ms / 1000);
@@ -81,18 +100,32 @@ const sleep = (ms: number) => {
 
 const queue = new AsyncRequestQueue({ maxConcurrent: 3, retries: 3 });
 
-queue.enqueue(async () => {
-  return sleep(1000);
-});
+queue.enqueue(
+  async () => {
+    return sleep(1000);
+  },
+  {
+    onSuccess: (value) => {
+      console.log("Sleep 1000 success", value);
+    },
+  }
+);
 queue.enqueue(async () => {
   return sleep(10000);
 });
 queue.enqueue(async () => {
   return sleep(3000);
 });
-queue.enqueue(async () => {
-  return Promise.reject("aa");
-});
+queue.enqueue(
+  async () => {
+    return Promise.reject("aa");
+  },
+  {
+    onError: (err) => {
+      console.log("Reject aa on error", err);
+    },
+  }
+);
 queue.enqueue(async () => {
   return sleep(1500);
 });
